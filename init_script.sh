d67755134525e496dd97a63381f01016c4268c20
Ameya Shenoy <shenoy.ameya@gmail.com>
2026-04-21 23:47:52 +0530

    feat: minimal dev init
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

---
+++ init_script.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+frappe_container_name=frappe-temp
+
+docker create --name "$frappe_container_name" codingcoffee/frappe-mono:develop
+docker cp "$frappe_container_name:/home/frappe/frappe-bench/apps" ./apps
+docker cp "$frappe_container_name:/home/frappe/frappe-bench/sites" ./sites
+docker cp "$frappe_container_name:/home/frappe/frappe-bench/env" ./.venv
+docker rm "$frappe_container_name"
+
